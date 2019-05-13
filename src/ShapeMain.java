d0762ca4d54089c6b373d71bcf06533a39fbe97d
Artur Przyjemski <spinezpl@gmail.com>
2019-05-13 21:49:43 +0200

    First version commit

--- src/ShapeMain.java
+++ src/ShapeMain.java
@@ -3,16 +3,16 @@ public class ShapeMain {
         ShapeCalculator shape = new ShapeCalculator();
 
         Square sguare = new Square(5,10);
-        System.out.println(sguare.sguareArea());
+        System.out.println(shape.sguareArea(sguare));
 
         Rectangle rectangle = new Rectangle(5,10);
-        System.out.println(rectangle.rectanglePerimeter());
+        System.out.println(shape.rectPerimeter(rectangle));
 
         Circle circle = new Circle(5.0);
-        System.out.println(circle.circleArea());
+        System.out.println(shape.circleArea(circle));
 
         Triangle triangle = new Triangle(5, 10, 15);
-        System.out.println(triangle.trianglePerimeter());
+        System.out.println(shape.trianglePerimeter(triangle));
 
 
 
