d3733d53cd1305e451222daaa5fcba478ee92e0d
Artur Przyjemski <spinezpl@gmail.com>
2019-05-13 21:54:25 +0200

    Version 2.0 ShapeCalculator switched OFF

--- src/ShapeCalculator.java
+++ src/ShapeCalculator.java
@@ -1,21 +1,21 @@
-public class ShapeCalculator {
-
-    double sguareArea(Square sguare){
-        double result = sguare.sguareArea();
-        return result;
-    }
-    double rectPerimeter(Rectangle rectangle){
-        double result = rectangle.rectanglePerimeter();
-        return result;
-    }
-
-    double circleArea(Circle circle){
-        double result = circle.circleArea();
-        return result;
-    }
-
-    double trianglePerimeter(Triangle triangle){
-        double result = triangle.trianglePerimeter();
-        return result;
-    }
-}
+//public class ShapeCalculator {
+//
+//    double sguareArea(Square sguare){
+//        double result = sguare.sguareArea();
+//        return result;
+//    }
+//    double rectPerimeter(Rectangle rectangle){
+//        double result = rectangle.rectanglePerimeter();
+//        return result;
+//    }
+//
+//    double circleArea(Circle circle){
+//        double result = circle.circleArea();
+//        return result;
+//    }
+//
+//    double trianglePerimeter(Triangle triangle){
+//        double result = triangle.trianglePerimeter();
+//        return result;
+//    }
+//}
--- src/ShapeMain.java
+++ src/ShapeMain.java
@@ -1,18 +1,18 @@
 public class ShapeMain {
     public static void main(String[] args) {
-        ShapeCalculator shape = new ShapeCalculator();
-
+//        ShapeCalculator shape = new ShapeCalculator();
+//
         Square sguare = new Square(5,10);
-        System.out.println(shape.sguareArea(sguare));
+        System.out.println(sguare.sguareArea());
 
         Rectangle rectangle = new Rectangle(5,10);
-        System.out.println(shape.rectPerimeter(rectangle));
+        System.out.println(rectangle.rectanglePerimeter());
 
         Circle circle = new Circle(5.0);
-        System.out.println(shape.circleArea(circle));
+        System.out.println(circle.circleArea());
 
         Triangle triangle = new Triangle(5, 10, 15);
-        System.out.println(shape.trianglePerimeter(triangle));
+        System.out.println(triangle.trianglePerimeter());
 
 
 
